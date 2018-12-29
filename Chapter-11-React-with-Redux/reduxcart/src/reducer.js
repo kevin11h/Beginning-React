f7e5ddbd8ca0116d1c1936e108afb57488dd8ba9
kevin11h <kevhung11@gmail.com>
2018-12-29 02:46:15 -0800

    finished Learning React book

---
+++ Chapter-11-React-with-Redux/reduxcart/src/reducer.js
@@ -0,0 +1,36 @@
+// Reducer
+function cartReducer(state, action) {
+	if (state === undefined) {
+		return {
+			totalCost: 0,
+			productCart: []
+		};
+	}
+
+	switch (action.type) {
+		case "addProduct":
+			return {
+				...state,
+				totalCost: state.totalCost + 
+					parseInt(action.productData.productPrice),
+				productCart: state.productCart.concat({
+					productName: action.productData.productName,
+					productPrice: action.productData.productPrice
+				})
+			}
+	
+		case "deleteProduct":
+			const udpatedArray = state.productCart.filter(product =>
+					product.productName !== action.productData.productName);
+			return {
+				...state,
+				totalCost: state.totalCost-parseInt(action.productData.productPrice),
+				productCart: udpatedArray
+			}
+		
+		default:
+		  	return state;
+	}
+}
+
+export default cartReducer;
